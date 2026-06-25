d091dde971adad9c856f6413d5d4912704bce539
Francis <37955960+Slendersdf@users.noreply.github.com>
2026-06-25 18:55:31 +0200

    Update source.c

--- level6/source.c
+++ level6/source.c
@@ -4,21 +4,21 @@ void n(void)
   return;
 }
 
-void m(void *param_1,int param_2,char *param_3,int param_4,i nt param_5)
+void m(void *param_1,int param_2,char *param_3,int param_4, int param_5)
 {
   puts("Nope");
   return;
 }
 
-void main(undefined4 param_1,int param_2)
+void main(int ac, char **av)
 {
   char *__dest;
-  undefined4 *puVar1;
+  void *puVar1;
   
-  __dest = malloc(0x40);
-  puVar1 = malloc(4);
+  __dest = malloc(0x40); //64
+  puVar1 = malloc(4); //4
   *puVar1 = m;
-  strcpy(__dest,*(char **)(param_2 + 4));
-  (*(code *)*puVar1)();
+  strcpy(__dest,*(char **)(param_2 + 4)); //copy av[1] (cause +4 = next argument,) to __dest
+  (*(code *)*puVar1)(); //m function call
   return;
 }
